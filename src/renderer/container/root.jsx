5d4d1f2d67e0fc9d87400dc626bb00dd9c474bff
keqh <keqh.keqh@gmail.com>
2015-10-30 22:42:07 +0900

    起動時にmeetups repoのcloneとpullを行う
    起動時に必要な情報が揃うよう、repositoryの情報を
    最新にする処理を仕込みました

--- src/renderer/container/root.jsx
+++ src/renderer/container/root.jsx
@@ -6,11 +6,7 @@ import Profile from '../component/profile';
 export default class Root extends React.Component {
     constructor(props) {
         super(props);
-        // 起動時にmeetupsのrepoを取得する
-        // NOTE: 開発中に再起動するたびfetchするのはあれなので、
-        // 落ち着くまで一旦comment outしておく
-        //this.props.actions.profiles.fetch();
-        this.props.actions.profiles.get();
+        this.props.actions.profiles.fetch();
     }
 
     render() {
