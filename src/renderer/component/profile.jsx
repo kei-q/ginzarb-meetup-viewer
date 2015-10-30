e4ef8ba0bb091070d3444727695c2839d84e68a2
keqh <keqh.keqh@gmail.com>
2015-10-30 22:42:07 +0900

    プロジェクタの画面に合わせてfont-sizeを変更できる
    毎回同じ環境とは限らないので、状況に応じて
    読みやすい文字の大きさに変更できるようにする

--- src/renderer/component/profile.jsx
+++ src/renderer/component/profile.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import Mousetrap from 'mousetrap';
 
 global.React = React;
 const md2react = require('md2react');
 
 export default class Profile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fontSize: 100
+        };
+    }
+
+    componentDidMount() {
+        Mousetrap.bind('+', () => {
+            this.setState({fontSize: this.state.fontSize + 10});
+        });
+        Mousetrap.bind('-', () => {
+            this.setState({fontSize: this.state.fontSize - 10});
+        });
+    }
+
+    componentWillUnmount() {
+        Mousetrap.unbind('+');
+        Mousetrap.unbind('-');
+    }
+
     render() {
         const markdownContents = md2react(this.props.profile);
+        const style = {
+            "font-size": `${this.state.fontSize}%`
+        };
 
-        return <div className="pane">
+        return <div className="pane" style={style}>
             <div className="markdown">
                 {markdownContents}
             </div>
